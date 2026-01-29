afdd35f3624b4782ad4b79f48fe47cbb654d9866
Diyora Vansh Ashokbhai MAQ Software <diyorav@maqsoftware.com>
2026-01-30 01:12:28 +0530

    Change form

--- src/features/vendors/components/AddVendorModal.tsx
+++ src/features/vendors/components/AddVendorModal.tsx
@@ -134,7 +134,7 @@ export default function AddVendorModal({
         { label: "Zoho", value: "zoho" },
         { label: "HubSpot", value: "hubspot" },
         { label: "Salesforce", value: "salesforce" },
-        { label: "Custom", value: "custom" },
+        { label: "Odoo", value: "odoo" },
       ],
     },
   ];
