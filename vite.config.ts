2b993f55d0cff5e15c40ab2305be4de589c30558
Diyora Vansh Ashokbhai MAQ Software <diyorav@maqsoftware.com>
2026-02-10 22:03:28 +0530

    optimise

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,20 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  build: {
+    sourcemap: false,
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes("node_modules")) {
+            if (id.includes("react")) return "react";
+            if (id.includes("react-router")) return "router";
+            return "vendor";
+          }
+        },
+      },
+    },
+  },
+});
