969430875b3903a02eb315db779203c4bd50b9cb
Diyora Vansh Ashokbhai MAQ Software <diyorav@maqsoftware.com>
2026-02-10 10:11:16 +0530

    changes

--- src/features/auth/pages/LoginPage.tsx
+++ src/features/auth/pages/LoginPage.tsx
@@ -13,13 +13,7 @@ export default function LoginPage() {
 
     // ▲ VERCEL PREVIEW / PROD DOMAINS
     if (hostname.endsWith(".vercel.app")) {
-      if(subdomain){
-
-        return `${protocol}//${subdomain}.${hostname}/${route}`;
-      }
-      else{
-        return `${protocol}//${hostname}/${route}`;
-      }
+      return `${protocol}//${hostname}/${route}`;
     }
 
     // 🌍 CUSTOM DOMAIN (e.g. zomato.com)
